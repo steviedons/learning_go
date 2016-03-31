8d8339105467699c6cb1b39c3787bd79261dfa04
Stephen Donovan <steviedons@gmail.com>
2016-03-31 15:21:04 +0100

    removed vim and vimrc setup from the dotfiles setup

--- dotfile_update.sh
+++ dotfile_update.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim tmux.conf tmux bash_profile bash-it"    # list of files/folders to symlink in homedir
+files="bashrc tmux.conf tmux bash_profile bash-it"    # list of files/folders to symlink in homedir
 
 ##########
 
