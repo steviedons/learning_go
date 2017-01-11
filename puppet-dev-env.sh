0c8b5dff12537f8eb8222d6f9d78dbd078af793d
Stephen Donovan <steviedons@gmail.com>
2017-01-11 13:45:33 +0000

    More changes to puppet env and tmux.conf to deal with tmux powerline being in a different place

--- puppet-dev-env.sh
+++ puppet-dev-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmux new-session -d 'vim'
+tmux new-session -d 
 tmux split-window -v 'ssh master.steviedons.com'
-tmux split-window -h
+tmux split-window -h 'vagrant ssh'
 tmux -2 attach-session -d
