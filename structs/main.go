2c2e010db57dac4813cacb25714888eb6d320f33
Stephen Donovan <steviedons@gmail.com>
2018-01-20 11:15:30 +0000

    created a print that didn't work until a pointer was used to pass in the person

--- structs/main.go
+++ structs/main.go
@@ -11,7 +11,10 @@ type person struct {
 	firstName string
 	lastName  string
 	age       int
-	contact   contactInfo
+	// contact   contactInfo
+	// This can also setup as
+	contactInfo
+	// and means the same as contactInfo contactInfo
 	// Added these to myself not sure the date of birth is correct
 	// dob time.Time
 }
@@ -25,7 +28,7 @@ func main() {
 		firstName: "Tracy",
 		lastName:  "Donovan",
 		age:       39,
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:    "trixiedons@gmail.com",
 			postcode: "GU73EE",
 		},
@@ -33,14 +36,32 @@ func main() {
 	// another way, create the person with every value set to zero values
 	var jack person
 
-	fmt.Println(steve, tracy)
-
-	// To print out the struct in a different way that will include the field names
-	fmt.Printf("%+v\n", jack)
+	// fmt.Println(steve, tracy)
 
 	jack.firstName = "Jack"
 	jack.lastName = "Donovan"
 	jack.age = 8
 
-	fmt.Println(jack)
+	// fmt.Println(jack)
+	// steve.updateName("steviedons")
+	//steve.print()
+	// need to use a pointer here to make sure steve is changed
+
+	stevePointer := &steve
+	stevePointer.updateName("Steve")
+	steve.print()
+
+	tracy.print()
+	jack.print()
+}
+
+// This code needs to be changed as it does not update the original object as its sent as a new object, need to
+// change it to use pointers
+func (pointerToPerson *person) updateName(newFirstName string) {
+	pointerToPerson.firstName = newFirstName
+}
+
+func (p person) print() {
+	// To print out the struct in a different way that will include the field names
+	fmt.Printf("%+v\n", p)
 }
